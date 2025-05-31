66c33ed73f2f0472d529da482fdde63316dd1f03
eugene <espizhovyi@gmail.com>
2025-05-30 21:25:55 -0700

    first commit

---
+++ Movement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrength = 100f;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+        rotation.Enable();
+    }
+    
+    private void FixedUpdate()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+}
